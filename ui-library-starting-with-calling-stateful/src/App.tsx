d70b1ebc661c6ed8c3320c6c0f92345c2faa55ea
Sérgio Orrù <sergio@top365.io>
2022-09-23 14:20:44 -0300

    Adição de paremetrização para PoC passanado os parametros de conexão e joinmeeting url na querystring.

--- ui-library-starting-with-calling-stateful/src/App.tsx
+++ ui-library-starting-with-calling-stateful/src/App.tsx
@@ -12,12 +12,16 @@ import React, { useEffect, useState } from 'react';
 import CallingComponents from './CallingComponentsStateful';
 import { registerIcons } from '@fluentui/react';
 import { Call, CallAgent } from '@azure/communication-calling';
+import { CommunicationIdentityClient } from '@azure/communication-identity';
+import { v4 as uuidv4 } from 'uuid';
 
 function App(): JSX.Element {
-  const userAccessToken = '<Azure Communication Services Resource Access Token>';
-  const userId = '<User Id associated to the token>';
-  const groupId = '<Generated GUID groupd id>';
-  const displayName = '<Display Name>';
+  var searchParameters = new URLSearchParams(window.location.search);
+  
+  let connectionString = atob(searchParameters.get('c') ?? '');
+  let meetingJoinUrl = searchParameters.get('j') ?? '';
+  let displayName =  searchParameters.get('un') ?? 'Robert Tolbert';
+  let userId = searchParameters.get('uid') ?? uuidv4() ?? 'cd0828df-f463-442a-87cd-b5e175d32d9a';
 
   registerIcons({ icons: DEFAULT_COMPONENT_ICONS });
 
@@ -32,20 +36,44 @@ function App(): JSX.Element {
   }, []);
 
   useEffect(() => {
-    const tokenCredential = new AzureCommunicationTokenCredential(userAccessToken);
-    if (callAgent === undefined && statefulCallClient && displayName) {
-      const createUserAgent = async () => {
-        setCallAgent(await statefulCallClient.createCallAgent(tokenCredential, { displayName: displayName }))
+    const initData = async (): Promise<void> => {
+
+      if (connectionString == '')
+      {
+        return;
       }
-      createUserAgent();
+
+      const identityClient = new CommunicationIdentityClient(connectionString);
+      let identityTokenResponse = await identityClient.createUserAndToken(["voip", "chat"]);
+      const { token, expiresOn, user } = identityTokenResponse;
+      const tokenCredential = new AzureCommunicationTokenCredential(token);
+      if (callAgent === undefined && statefulCallClient && displayName) {
+        const createUserAgent = async () => {
+          setCallAgent(await statefulCallClient.createCallAgent(tokenCredential, { displayName: displayName }))
+        }
+        createUserAgent();
+      }
     }
+
+    initData();
   }, [statefulCallClient, displayName, callAgent]);
 
   useEffect(() => {
-    if (callAgent !== undefined) {
-      setCall(callAgent.join({ groupId }));
+    if (callAgent !== undefined && meetingJoinUrl != '') {
+      setCall(callAgent.join({ meetingLink: meetingJoinUrl }));
     }
   }, [callAgent]);
+
+  if (connectionString === '' || meetingJoinUrl === '')
+  {
+    return(
+      <>
+        <FluentThemeProvider>
+          <h1>Invalid parameters.</h1>
+        </FluentThemeProvider>
+      </>
+    )
+  }
 
   return (
     <>
--- ui-library-starting-with-calling-stateful/src/CallingComponentsStateful.tsx
+++ ui-library-starting-with-calling-stateful/src/CallingComponentsStateful.tsx
@@ -1,5 +1,6 @@
-import { usePropsFor, VideoGallery, ControlBar, CameraButton, MicrophoneButton, ScreenShareButton, EndCallButton, useCall } from '@azure/communication-react';
+import { usePropsFor, VideoGallery, ControlBar, CameraButton, MicrophoneButton, ScreenShareButton, EndCallButton, useCall, useCallAgent } from '@azure/communication-react';
 import { mergeStyles, Stack } from '@fluentui/react';
+import { Button, Input } from "@fluentui/react-northstar";;
 
 function CallingComponents(): JSX.Element {
 
@@ -10,10 +11,25 @@ function CallingComponents(): JSX.Element {
   const endCallProps = usePropsFor(EndCallButton);
 
   const call = useCall();
+  const callAgent = useCallAgent();
+
+  let meetingJoinUrl = '';
+
+  console.log("call data:")
+  console.log(call);
 
   if (call?.state === 'Disconnected') {
     return <CallEnded />;
   }
+  
+  console.log("Microphone Options: ");
+  console.log(microphoneProps);
+
+  console.log("Camera Options: ");
+  console.log(cameraProps);
+
+  console.log("Call Agent:")
+  console.log(callAgent);
 
   return (
     <Stack className={mergeStyles({ height: '100%' })}>
@@ -23,13 +39,15 @@ function CallingComponents(): JSX.Element {
 
       <ControlBar layout='floatingBottom'>
         {cameraProps && <CameraButton  {...cameraProps} />}
-        {microphoneProps && <MicrophoneButton   {...microphoneProps} />}
+        {microphoneProps && <MicrophoneButton  {...microphoneProps} />}
         {screenShareProps && <ScreenShareButton  {...screenShareProps} />}
         {endCallProps && <EndCallButton {...endCallProps} />}
       </ControlBar>
     </Stack>
   );
 }
+
+
 
 function CallEnded(): JSX.Element {
   return <h1>You ended the call.</h1>;
